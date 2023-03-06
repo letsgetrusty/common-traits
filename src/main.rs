240a6dffe8ed70cf770e5fbae88fcdee5ec32198
letsgetrusty <bogdan@letsgetrusty.com>
2023-03-06 12:52:08 -0600

    init

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use std::{rc::Rc, sync::Arc};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Default, PartialEq, Serialize, Deserialize)]
+pub enum Role {
+    Admin,
+    Standard,
+    #[default]
+    Guest,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Serialize, Deserialize)]
+struct DB {}
+
+#[derive(Debug, Clone, Default, PartialEq, Serialize, Deserialize)]
+pub struct User {
+    id: u32,
+    name: String,
+    role: Role,
+    #[serde(skip)]
+    db: Arc<DB>,
+}
+
+fn main() {
+    let user = User {
+        id: 123,
+        name: "Bogdan".to_owned(),
+        role: Role::Admin,
+        db: Arc::new(DB {}),
+    };
+
+    println!("{:?}", user);
+
+    let user2 = user.clone();
+
+    println!("{:?}", user2);
+
+    let guest = User::default();
+
+    let guest2 = User::default();
+
+    assert_eq!(guest, guest2);
+
+    let user_str = "{ \"id\": 123, \"name\": \"Bogdan\", \"role\": \"Admin\" }";
+
+    let user: User = serde_json::from_str(&user_str).unwrap();
+
+    println!("{:?}", user);
+}
+
+fn is_normal<T: Sized + Send + Sync + Unpin>() {}
+
+#[test]
+fn normal_types() {
+    is_normal::<User>();
+}
